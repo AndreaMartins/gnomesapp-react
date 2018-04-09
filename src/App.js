fa93ff7ca212dda4f0caeef46d3b630ae199f637
Andrea Martins <marlit8@gmail.com>
2018-04-09 23:52:23 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Link, Switch } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import './App.css';
 import Lists from './Lists/Lists';
@@ -8,15 +8,13 @@ import Detail from './Detail/Detail';
 
 class App extends Component {
   render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/:id" exact component={Detail} />
-          <Route path="/" exact component={Lists} />
+    return (<BrowserRouter>
+      <Switch>
+        <Route path="/:id" exact component={Detail}/>
+        <Route path="/" exact component={Lists}/>
 
-        </Switch>
-      </BrowserRouter>
-    );
+      </Switch>
+    </BrowserRouter>);
   }
 }
 
--- src/Lists/Lists.js
+++ src/Lists/Lists.js
@@ -1,86 +1,64 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-// import axios from '../axios';
-
-import { Link } from 'react-router-dom';
 import './Lists.css';
 
 class Lists extends Component {
 
-//receive the list of gnomes
   state = {
-    gnomes:[],
-    input: '',
+    gnomes: [],
+    input: ''
   }
 
-//get all the list of gnomes
-
-  componentDidMount () {
-      axios.get('https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json')
-           .then(response => {
-              this.setState({gnomes: response.data.Brastlewark});
-              console.log(this.state.gnomes)
-          });
+  componentDidMount() {
+    axios.get('https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json').then(response => {
+      this.setState({gnomes: response.data.Brastlewark});
+      console.log(this.state.gnomes)
+    });
   }
-
 
   gnomeSelectedHandler = (id) => {
     this.props.history.push('/' + id);
   }
 
-  onChangeHandler(e){
-    this.setState({
-      input: e.target.value,
-    })
+  onChangeHandler(e) {
+    this.setState({input: e.target.value})
   }
 
-  render () {
-      const gnomes = this.state.gnomes
-      .filter(gnome => gnome.age == this.state.input || this.state.input == '')
-      .map(gnome => {
-          return(
-          <div key={gnome.id} className="containergnome" onClick={ () => this.gnomeSelectedHandler(gnome.id)} >
-            <div className="containergnome__item">
-              <figure className="containergnome__shape">
-                <img src={gnome.thumbnail} className="containergnome__img"/>
+  render() {
+    const gnomes = this.state.gnomes.filter(gnome => gnome.age == this.state.input || this.state.input === '').map(gnome => {
+      return (<div key={gnome.id} className="containergnome" onClick={() => this.gnomeSelectedHandler(gnome.id)}>
+        <div className="containergnome__item">
+          <figure className="containergnome__shape">
+            <img src={gnome.thumbnail} className="containergnome__img" alt=""/>
 
-                <figcaption className="containergnome__caption"> + details</figcaption>
+            <figcaption className="containergnome__caption">
+              + details</figcaption>
 
-              </figure>
-            </div>
-            <div>
-              <p className="containergnome__font">{gnome.name}</p>
-            </div>
-          </div>
-          )
-      });
-
-      return (
+          </figure>
+        </div>
         <div>
-          <nav className="navgnome">
-            <div className="navgnome__container">
-              <h1>
-                Brastlewark!
-              </h1>
-              <div className="search">
-                <input value={this.state.input} type="text" onChange={this.onChangeHandler.bind(this)} placeholder="  Search Gnome by age" />
-              </div>
-            </div>
-          </nav>
-          <div className="listcontainer">
-            {gnomes}
+          <p className="containergnome__font">{gnome.name}</p>
+        </div>
+      </div>)
+    });
+
+    return (<div>
+      <nav className="navgnome">
+        <div className="navgnome__container">
+          <h1>
+            Brastlewark!
+          </h1>
+          <div className="search">
+            <input value={this.state.input} type="text" onChange={this.onChangeHandler.bind(this)} placeholder="  Search Gnome by age"/>
           </div>
         </div>
-      );
+      </nav>
+      <div className="listcontainer">
+        {gnomes}
+      </div>
+    </div>);
   }
-
-
-
 
 }
 
 export default Lists;
-
-
-
-// Task to render all the gnomes, get the dat from the backend by the use of the service and to filter
